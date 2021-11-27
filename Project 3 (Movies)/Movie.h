ec1df16339acf10fb0e6de53f55332aa59b990d1
peterlucchino <peter.lucchino@ufl.edu>
2021-11-27 15:39:06 -0500

    Added Movie class

---
+++ Project 3 (Movies)/Movie.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<string>
+using namespace std;
+class Movie
+{
+	string title;
+	string genre;
+	int year;
+
+	Movie() {
+		title = "";
+		genre = "";
+		year = 0;
+	}
+	Movie(string theTitle, string theGenre, int theYear) {
+		title = theTitle;
+		genre = theGenre;
+		year = theYear;
+	}
+
+	//insert graphs here
+};
